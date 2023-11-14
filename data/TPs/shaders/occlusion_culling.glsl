d9259dc6eadeef76378d4ef46f1a624322823913
Tom Clabault <email>
2023-11-14 18:25:57 +0100

    TP4 Compute Shaders done

--- data/TPs/shaders/occlusion_culling.glsl
+++ data/TPs/shaders/occlusion_culling.glsl
@@ -2,14 +2,15 @@
 
 #ifdef COMPUTE_SHADER
 
-layout(std430, binding = 0) buffer inp
+layout(std430, binding = 0) buffer inputData
 {
-    int input_data[];
+	int global_counter;
+	int input_data[];
 };
 
-layout(std430, binding = 1) buffer outp
+layout(std430, binding = 1) buffer bufferOutputData
 {
-    int output_data[];
+	int output_data[];
 };
 
 uniform int u_operand;
@@ -17,12 +18,16 @@ uniform int u_operand;
 layout(local_size_x = 256) in;
 void main()
 {
-    uint id = gl_GlobalInvocationID.x;
+	const uint thread_id = gl_GlobalInvocationID.x;
 
-    // chaque thread transforme le sommet d'indice ID...
-    if(id < input_data.length())
-        output_data[id] = u_operand + input_data[id];
+	if (thread_id < input_data.length())
+	{
+		int value = input_data[thread_id] + u_operand;
+		int counter = atomicAdd(global_counter, 1);
+
+		if (value >= 100)
+			output_data[counter] = value;
+	}
 }
 
 #endif
-
